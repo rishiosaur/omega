17f1a7550fb752ab82ad1a700d911684ff4caad8
cherche <contactryannguyen@gmail.com>
2016-01-01 10:36:04 -0500

    The base of fuchsia has been built
    Added display
    Added autoscroll*
    Added basic code to control fuchsia**
    
    *Autoscroll seems to break fuchsia on smaller displays
    **fuchsia can greet you and clear the log

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+$(function () {
+	var y, r;
+	$('input').keydown(function (e) {
+		if (e.which === 13 && $(this).val() !== "") {
+			y = $(this).val().charAt(0).toUpperCase() + $(this).val().slice(1);
+			$(this).val('').blur();
+			$('<div class="conversation you">' + y + '</div>').appendTo('#conversation-box').hide().fadeIn('slow', function () {
+				app(y);
+			});
+		}
+	});
+	function app (y) {
+		y = y.toLowerCase();
+		y = y.split('?').join('');
+		if (y === 'clear log' || y === 'clear logs') {
+			say('Clearing log...');
+			setTimeout(function () {
+				$('#conversation-box').fadeOut('slow', function () {
+					$('#conversation-box').empty().show();
+				});
+			}, 1000);
+		} else if (y === 'hi' || y === 'hello' || y === 'hey') {
+			say(['Hi', 'Hello', 'Hey', 'Greetings'][Math.floor(Math.random() * 4)]);
+		}
+	}
+	function say (r) {
+		$('<div class="conversation fuchsia">' + r + '</div>').appendTo('#conversation-box').hide().fadeIn('slow', function () {
+			if ($('#conversation-box > .conversation').length > 2) {
+				$('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+			}
+		});
+	}
+});
