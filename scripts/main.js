42d8c8cee6018b864ddf44cf69c05edbec6cd889
cherche <contactryannguyen@gmail.com>
2016-01-02 18:05:47 -0500

    Optimized time

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,20 @@ $(function () {
 	$('.toggle').click(function () {
 		$(this).parent().children('img').toggle();
 	});
+	/* Take input from text box */
 	$('input').keydown(function (e) {
 		if (e.which === 13 && $(this).val() !== "") {
 			y = $(this).val().charAt(0).toUpperCase() + $(this).val().slice(1);
+			y.split('<').join('&lt;');
 			$(this).val('').blur();
 			$('<div class="conversation you">' + y + '</div>').appendTo('#conversation-box').fadeIn('slow', function () {
-				$('html, body').animate({scrollTop: $(document).height()}, 'slow');
+				y.split('&lt;').join('<');
 				app(y);
 			});
 		}
 	});
 	function app (y) {
+		/* Parse string */
 		y = y.toLowerCase();
 		y = y.split('?').join('');
 		y = y.split('!').join('');
@@ -28,6 +31,7 @@ $(function () {
 		if (y.slice(-1) === '.') {
 			y = y.slice(0, y.length - 1);
 		}
+		/* Offline Features */
 		if (y === 'clear' || y === 'clear log' || y === 'clear logs') {
 			say('Clearing log&hellip;');
 			setTimeout(function () {
@@ -56,6 +60,50 @@ $(function () {
 			}
 		} else if (y.startsWith('you')) {
 			say(['Thank you', 'That\'s what I thought', 'We should be talking more about you'][Math.floor(Math.random() * 3)] + ['.', '!'][Math.floor(Math.random() * 2)]);
+		} else if (y === 'what is the time' || y === 'what time is it' || y === 'give me the time' || y.split('day').join('date') === 'what is the date' || y.split('day').join('date') === 'what date is it' || y.split('day').join('date') === 'give me the date' || y.split('day').join('date') === 'what is the time and date' || y.split('day').join('date') === 'what is the date and time' || y.split('day').join('date') === 'what is the time and the date' || y.split('day').join('date') === 'what is the date and the time' || y.split('day').join('date') === 'what time and date is it' || y.split('day').join('date') === 'what date and time is it' || y.split('day').join('date') === 'give me the time and date' || y.split('day').join('date') === 'give me the date and time' || y.split('day').join('date') === 'give me the time and the date' || y.split('day').join('date') === 'give me the date and the time' || y.split('day').join('date') === 'when am i') {
+			var now = new Date();
+			var date, time, suffix;
+			y = (y === 'when am i') ? 'tid' : y;
+			if (y.indexOf('ti') !== -1) {
+				time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+				suffix = (time[0] < 12) ? ' AM' : ' PM';
+				time[0] = (time[0] <= 12) ? time[0] : time[0] - 12;
+				time[1] = (time[1] >= 10) ? time[1] : '0' + time[1];
+				time[2] = (time[2] >= 10) ? time[2] : '0' + time[2];
+				time = time.join(':');
+			}
+			if (y.indexOf('d') !== -1) {
+				var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+				var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+				var chron = ['st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+				var tempdate;
+				date = [now.getDay(), now.getMonth(), now.getDate(), now.getFullYear()];
+				date[0] = days[date[0]];
+				date[1] = months[date[1]];
+				tempdate = date[2];
+				tempdate = (tempdate >= 10) ? tempdate.toString() : '0' + tempdate;
+				console.log(tempdate);
+				if (tempdate[0] === '1') {
+					date[2] += 'th,';
+					console.log(date[2]);
+				} else {
+					date[2] += chron[parseInt(tempdate[1]) - 1] + ',';
+				}
+				date = date.join(' ');
+			}
+			if (y.indexOf('ti') !== -1 && y.indexOf('d') !== -1) {
+				say('It is currently <b>' + date + ' ' + time + suffix + '</b>.');
+			} else if (y.indexOf('time') !== -1) {
+				say('The time is <b>' + time + suffix + '</b>.');
+			} else {
+				say('The date is <b>' + date + '</b>.');
+			}
+		} else if (y === 'toggle goodnight' || y === 'flip goodnight' || y === 'switch goodnight' || y === 'goodnight.toggle();' || y === 'goodnight.toggle()') {
+			say('Toggling Goodnight&mdash;brace yourself&hellip;');
+			setTimeout(function () {
+				Goodnight.toggle();
+			}, 2000);
+		/* Online Features */
 		} else if (y.startsWith('search for ')){
 			newtab = y.slice(11);
 			say('Searching Google for "' + newtab + '"&hellip;');
@@ -101,15 +149,19 @@ $(function () {
 			} else {
 				$.getJSON('https://api.spotify.com/v1/search?q=' + y.slice(19).split(' ').join('+') + '&type=artist&limit=1', function (d) {
 					var image;
+					var genres = '';
 					for (var i = 0;i < d.artists.items[0].genres.length;i++) {
-						d.artists.items[0].genres[i] = 'Genres: ' + d.artists.items[0].genres[i].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+						d.artists.items[0].genres[i] = d.artists.items[0].genres[i].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+					}
+					if (d.artists.items[0].genres[0] !== undefined) {
+						genres = 'Genres: ';
 					}
 					if (d.artists.items[0].images[0].url !== undefined) {
 						image = '<p>[<a class="toggle">Show/Hide Image</a>]<img src="' + d.artists.items[0].images[0].url + '"></p>';
 					}
 					if (d.artists.total) {
 						say('Here is what I could find:');
-						$('<div class="box"><h1 class="title">' + d.artists.items[0].name + '</h1><p class="musictype">Music Artist</p><p class="rated">' + d.artists.items[0].genres.join(', ') + '</p>' + image + '<p class="reviews">Popularity on Spotify: ' + d.artists.items[0].popularity + '<br>Followers on Spotify: ' + d.artists.items[0].followers.total + '</p></div>').appendTo('#conversation-box').fadeIn('slow');
+						$('<div class="box"><h1 class="title">' + d.artists.items[0].name + '</h1><p class="musictype">Music Artist</p><p class="rated">' + genres + d.artists.items[0].genres.join(', ') + '</p>' + image + '<p class="reviews">Popularity on Spotify: ' + d.artists.items[0].popularity + '<br>Followers on Spotify: ' + d.artists.items[0].followers.total + '</p></div>').appendTo('#conversation-box').fadeIn('slow');
 					} else {
 						say('I couldn\'t find that artist. I\'ll perform a Google search for them instead.');
 						setTimeout(function () {
@@ -121,38 +173,14 @@ $(function () {
 					}
 				});
 			}
-		} else if (y === 'what is the time' || y === 'what time is it' || y === 'give me the time' || y === 'what is the date' || y === 'what date is it' || y === 'give me the date' || y === 'what is the time and date' || y === 'what is the date and time' || y === 'what is the time and the date' || y === 'what is the date and the time' || y === 'what time and date is it' || y === 'what date and time is it' || y === 'give me the time and date' || y === 'give me the date and time' || y === 'give me the time and the date' || y === 'give me the date and the time' || y === 'when am i') {
-			var now = new Date();
-			var date, time, suffix;
-			y = (y === 'when am i') ? 'time date' : y;
-			if (y.indexOf('time') !== -1) {
-				time = [now.getHours(), now.getMinutes(), now.getSeconds()];
-				suffix = (time[0] < 12) ? ' AM' : ' PM';
-				time[0] = (time[0] <= 12) ? time[0] : time[0] - 12;
-				time[1] = (time[1] >= 10) ? time[1] : '0' + time[1];
-				time[2] = (time[2] >= 10) ? time[2] : '0' + time[2];
-				time = time.join(':');
-			}
-			if (y.indexOf('date') !== -1) {
-				var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-				var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-				date = [now.getDay(), now.getMonth(), now.getDate() + ',', now.getFullYear()];
-				date[0] = days[date[0]];
-				date[1] = months[date[1]];
-				date = date.join(' ');
-			}
-			if (y.indexOf('time') !== -1 && y.indexOf('date') !== -1) {
-				say('It is currently <b>' + date + ' ' + time + suffix + '</b>.');
-			} else if (y.indexOf('time') !== -1) {
-				say('The time is <b>' + time + suffix + '</b>.');
-			} else {
-				say('The date is <b>' + date + '</b>.');
-			}
-		} else if (y === 'toggle goodnight' || y === 'flip goodnight' || y === 'switch goodnight') {
-			say('Toggling Goodnight&mdash;brace yourself&hellip;');
-			setTimeout(function () {
-				Goodnight.toggle();
-			}, 2000);
+		/* Easter Eggs */
+		} else if (y === 'ayy') {
+			say('lmao');
+		} else if (y === 'tell me a joke' || y.indexOf('chuck norris') !== -1 || y === 'be funny' || y === 'make me laugh') {
+			$.getJSON('http://api.icndb.com/jokes/random', function (d) {
+				say(d.value.joke);
+			});
+		/* Default Response */
 		} else {
 			say('I apologize, but I wasn\'t sure what you were asking of me. I\'ll perform a Google search instead.');
 			setTimeout(function () {
@@ -162,6 +190,7 @@ $(function () {
 				}, 2000);
 			}, 2000);
 		}
+		$('html, body').animate({scrollTop: $(document).height()}, 'slow');
 	}
 	function say (r) {
 		$('<div class="conversation fuchsia">' + r + '</div>').appendTo('#conversation-box').fadeIn('slow');
