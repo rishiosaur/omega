db0c6936b15063c14f1205ac5e855296a6df6cc0
cherche <contactryannguyen@gmail.com>
2016-01-03 19:24:26 -0500

    Modularized things; fixed show image bug; added random module

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,9 @@ $(function () {
 	FastClick.attach(document.body);
 	Goodnight.css('stylesheets/dark.css');
 	var y, newtab;
-	setTimeout(function () {
-		$('#container').fadeIn('slow');
-	}, 1000);
-	$('.toggle').click(function () {
-		$(this).parent().children('img').toggle();
+	$(document).on("click", ".toggle", function() {
+		$(this).parent().children('.toggle').toggle();
 	});
-	/* Take input from text box */
 	$('input').keydown(function (e) {
 		if (e.which === 13 && $(this).val() !== '') {
 			y = $(this).val().charAt(0).toUpperCase() + $(this).val().slice(1);
@@ -21,8 +17,14 @@ $(function () {
 			});
 		}
 	});
+	function say (r) {
+		$('<div class="conversation fuchsia">' + r + '</div>').appendTo('#conversation-box').fadeIn('slow');
+	}
 	function app (y) {
-		/* Parse string */
+		parse(y);
+		$('html, body').animate({scrollTop: $(document).height()}, 'slow');
+	}
+	function parse (y) {
 		y = y.toLowerCase();
 		y = y.split('?').join('');
 		y = y.split('!').join('');
@@ -31,7 +33,40 @@ $(function () {
 		if (y.slice(-1) === '.') {
 			y = y.slice(0, y.length - 1);
 		}
-		/* Offline Features */
+		random(y);
+	}
+	function random (y) {
+		if (y === 'flip a coin') {
+			say(['Heads', 'Tails'][Math.floor(Math.random() * 2)] + '.');
+		} else if (y === 'roll a die') {
+			say(['One', 'Two', 'Three', 'Four', 'Five', 'Six'][Math.floor(Math.random() * 6)] + '.');
+		} else if (y.startsWith('give me a random integer between ') || y.startsWith('give me a random number between ') || y.startsWith('generate a random integer between ') || y.startsWith('generate a random number between ')) {
+			if (y.indexOf('give') !== -1) {
+				if (y.indexOf('integer') !== -1) {
+					y = y.slice(33);
+				} else {
+					y = y.slice(32);
+				}
+			} else {
+				if (y.indexOf('integer') !== -1) {
+					y = y.slice(34);
+				} else {
+					y = y.slice(31);
+				}
+			}
+			y = y.split(' and ');
+			y[0] = parseFloat(y[0], 10);
+			y[1] = parseFloat(y[1], 10);
+			if (y[0] !== NaN && y[1] !== NaN) {
+				say(Math.floor(Math.random() * (Math.max(y[0], y[1]) - Math.min(y[0], y[1]) + 1) + Math.min(y[0], y[1])));
+			} else {
+				say('Those aren\'t both integers!');
+			}
+		} else {
+			core(y);
+		}
+	}
+	function core (y) {
 		if (y === 'clear' || y === 'clear log' || y === 'clear logs') {
 			say('Clearing log&hellip;');
 			setTimeout(function () {
@@ -74,7 +109,7 @@ $(function () {
 					date[2] += 'th,';
 					console.log(date[2]);
 				} else {
-					date[2] += chron[parseInt(tempdate[1]) - 1] + ',';
+					date[2] += chron[parseInt(tempdate[1], 10) - 1] + ',';
 				}
 				date = date.join(' ');
 			}
@@ -90,7 +125,6 @@ $(function () {
 			setTimeout(function () {
 				Goodnight.toggle();
 			}, 2000);
-		/* Online Features */
 		} else if (y.startsWith('go to ') || y.startsWith('open ')) {
 			if (y.startsWith('go to')) {
 				newtab = y.slice(6);
@@ -120,7 +154,7 @@ $(function () {
 				$.getJSON('https://www.omdbapi.com/?t=' + y + '&y=&plot=full&r=json&tomatoes=true', function (d) {
 					var image;
 					if (d.Poster !== undefined) {
-						image = '<p>[<a class="toggle">Show/Hide Poster</a>]<img src="' + d.Poster + '"></p>';
+						image = '<p><a class="toggle">[Show Poster]</a><img src="' + d.Poster + '" title="Click to hide" class="toggle"></p>';
 					}
 					if (d.Error === undefined) {
 						var reviews = '', sum = 0, count = 0;
@@ -131,24 +165,24 @@ $(function () {
 						}
 						if (d.imdbRating !== 'N/A') {
 							reviews += 'IMDb: ' + d.imdbRating + '/10<br>';
-							sum += parseFloat(d.imdbRating) * 10;
+							sum += parseFloat(d.imdbRating, 10) * 10;
 							count++;
 						}
 						if (d.tomatoRating !== 'N/A') {
 							reviews += 'Rotten Tomatoes: ' + d.tomatoRating + '/10<br>';
-							sum += parseFloat(d.tomatoRating) * 10;
+							sum += parseFloat(d.tomatoRating, 10) * 10;
 							count++;
 						}
 						if (d.Metascore !== 'N/A') {
 							reviews += 'Metacritic: ' + d.Metascore + '/100<br>';
-							sum += parseInt(d.Metascore);
+							sum += parseInt(d.Metascore, 10);
 							count++;
 						}
 						reviews += '<br>Average: ' + Math.round(sum / count) + '%';
 						say('Here is what I could find:');
 						$('<div class="box"><h1 class="title">' + d.Title + '</h1><p class="actors">Starring ' + d.Actors + '</p><p class="rated">' + d.Rated + '</p><p class="info">' + d.Year + ' ' + d.Type + '</p><p class="plot">' + d.Plot + '</p>' + image + '<p class="reviews">' + reviews + '</p></div>').appendTo('#conversation-box').fadeIn('slow');
 					} else {
-						say('I apologize, but I couldn\'t find that film. I\'ll perform a Googe search instead.');
+						say('I apologize, but I couldn\'t find that film. I\'ll perform a Google search instead.');
 						setTimeout(function () {
 							say('Searching Google for "' + y + '"&hellip;');
 							setTimeout(function () {
@@ -161,16 +195,16 @@ $(function () {
 				$.getJSON('https://api.spotify.com/v1/search?q=' + y.slice(19).split(' ').join('+') + '&type=artist&limit=1', function (d) {
 					var image;
 					var genres = '';
-					for (var i = 0;i < d.artists.items[0].genres.length;i++) {
-						d.artists.items[0].genres[i] = d.artists.items[0].genres[i].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-					}
-					if (d.artists.items[0].genres[0] !== undefined) {
-						genres = 'Genres: ';
-					}
-					if (d.artists.items[0].images[0].url !== undefined) {
-						image = '<p>[<a class="toggle">Show/Hide Image</a>]<img src="' + d.artists.items[0].images[0].url + '"></p>';
-					}
-					if (d.artists.total) {
+					if (d.artists.count) {
+						for (var i = 0;i < d.artists.items[0].genres.length;i++) {
+							d.artists.items[0].genres[i] = d.artists.items[0].genres[i].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+						}
+						if (d.artists.items[0].genres[0] !== undefined) {
+							genres = 'Genres: ';
+						}
+						if (d.artists.items[0].images[0].url !== undefined) {
+							image = '<p><a class="toggle">[Show Image]</a><img src="' + d.artists.items[0].images[0].url + '" title="Click to hide" class="toggle"></p>';
+						}
 						say('Here is what I could find:');
 						$('<div class="box"><h1 class="title">' + d.artists.items[0].name + '</h1><p class="musictype">Music Artist</p><p class="rated">' + genres + d.artists.items[0].genres.join(', ') + '</p>' + image + '<p class="reviews">Popularity on Spotify: ' + d.artists.items[0].popularity + '<br>Followers on Spotify: ' + d.artists.items[0].followers.total + '</p></div>').appendTo('#conversation-box').fadeIn('slow');
 					} else {
@@ -184,26 +218,26 @@ $(function () {
 					}
 				});
 			}
-		/* Easter Eggs */
 		} else if (y === 'ayy') {
 			say('lmao');
 		} else if (y === 'tell me a joke' || y.indexOf('chuck norris') !== -1 || y === 'be funny' || y === 'make me laugh') {
 			$.getJSON('http://api.icndb.com/jokes/random', function (d) {
 				say(d.value.joke);
 			});
-		/* Default Response */
 		} else {
-			say('I apologize, but I wasn\'t sure what you were asking of me. I\'ll perform a Google search instead.');
-			setTimeout(function () {
-				say('Searching Google for "' + y + '"&hellip;');
-				setTimeout(function () {
-					window.open('https://www.google.ca/search?q=' + y.split(' ').join('+'), '_blank');
-				}, 2000);
-			}, 2000);
+			fallback(y);
 		}
-		$('html, body').animate({scrollTop: $(document).height()}, 'slow');
 	}
-	function say (r) {
-		$('<div class="conversation fuchsia">' + r + '</div>').appendTo('#conversation-box').fadeIn('slow');
+	function fallback (y) {
+		say('I apologize, but I wasn\'t sure what you were asking of me. I\'ll perform a Google search instead.');
+		setTimeout(function () {
+			say('Searching Google for "' + y + '"&hellip;');
+			setTimeout(function () {
+				window.open('https://www.google.ca/search?q=' + y.split(' ').join('+'), '_blank');
+			}, 2000);
+		}, 2000);
 	}
+	setTimeout(function () {
+		$('#container').fadeIn('slow');
+	}, 1000);
 });
