27a66384fb8efa454b3b3a46e6d36d6e42bb18b8
cherche <contactryannguyen@gmail.com>
2016-01-30 19:07:32 -0500

    Updated main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -307,7 +307,7 @@ $(function () {
 				say(d.value.joke);
 			});
 		} else if (y.startsWith('i')) {
-			say(['Why you always lyin\'?', 'That\'s nice.'][Math.floor(Math.random() * 2)]);
+			say(['Why you always lyin\'?', 'Good for you.'][Math.floor(Math.random() * 2)]);
 		} else if (y.startsWith('you')) {
 			say(['Thank you', 'That\'s what I thought', 'We should be talking more about you'][Math.floor(Math.random() * 3)] + ['.', '!'][Math.floor(Math.random() * 2)]);
 		} else if (y.startsWith('never gonna give you up')) {
